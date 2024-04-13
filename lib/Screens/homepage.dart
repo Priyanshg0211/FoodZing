3ebda3e4ef2238f72ef985eceea401366f416a4f
Priyansh Gupta <priyansh.dev02@gmail.com>
2024-04-14 04:22:36 +0530

    added appbar icon and bottom navigation bar

--- lib/Screens/homepage.dart
+++ lib/Screens/homepage.dart
@@ -5,6 +5,46 @@ class homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(
+          Icons.menu,
+          color: Colors.grey,
+        ),
+        actions: <Widget>[
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Colors.grey,
+              )),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        fixedColor: Colors.black,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.food_bank_rounded,
+                color: Colors.black,
+              ),
+              label: 'Home'),
+        
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                color: Colors.black,
+              ),
+              label: 'Explore'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.account_circle_sharp,
+                color: Colors.black,
+              ),
+              label: 'User'),
+        ],
+      ),
+    );
   }
 }
