6493eab983c9d7b197c335c8720f73d2aa87f06e
Priyansh Gupta <priyansh.dev02@gmail.com>
2024-04-14 13:16:05 +0530

    added foods category and container of food menu

--- lib/Screens/homepage.dart
+++ lib/Screens/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class homepage extends StatelessWidget {
   const homepage({super.key});
@@ -20,6 +21,162 @@ class homepage extends StatelessWidget {
               )),
         ],
       ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            child: Text(
+              'Delicious\nfood for you',
+              style: TextStyle(
+                  fontFamily: 'SFPro',
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: Container(
+              height: 50,
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: TextField(
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                  fillColor: Colors.transparent,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  hintText: 'Search',
+                  hintStyle: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 14.0,
+                      color: Colors.grey),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.black,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Foods',
+                        style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 14.0,
+                            color: Colors.black),
+                      )),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Drinks',
+                        style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 14.0,
+                            color: Colors.black),
+                      )),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Snacks',
+                        style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 14.0,
+                            color: Colors.black),
+                      )),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Desserts',
+                      style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 14.0,
+                          color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Biryani',
+                      style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 14.0,
+                          color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Panner',
+                      style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 14.0,
+                          color: Colors.black),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.5,
+                width: MediaQuery.of(context).size.width * 0.7,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.white,
         fixedColor: Colors.black,
@@ -30,7 +187,6 @@ class homepage extends StatelessWidget {
                 color: Colors.black,
               ),
               label: 'Home'),
-        
           BottomNavigationBarItem(
               icon: Icon(
                 Icons.search,
